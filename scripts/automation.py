6fccb21983f369c8a222e3bf3a00bcf86cbb78fc
natandavinci <natanaelqueiroz969@gmail.com>
2026-05-16 09:57:31 -0300

    Building the automation - login

--- scripts/automation.py
+++ scripts/automation.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
 
 
 def open_browser():
@@ -8,7 +12,26 @@ def open_browser():
         service=Service(ChromeDriverManager().install())
     )
 
-    driver.get("https://dlp.hashtagtreinamentos.com/python/intensivao/tabela")
+    driver.get("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+
+    time.sleep(3)
+
+    #LOGIN - EMAIL
+    email = driver.find_element(By.NAME, "email")
+    email.click()
+    email.send_keys("bottest@gmail.com")
+    email.send_keys(Keys.RETURN)
+    
+    time.sleep(3)
+
+    #LOGIN - PASSWORD
+    password = driver.find_element(By.NAME, "password" )
+    password.click()
+    password.send_keys("123@#$456&abndBNM")
+    password.send_keys(Keys.RETURN)
+
+
+
 
     input("Press ENTER to close browser...")
 
